a076b2009fae378a6a33a20dfbd9fe5ead9d0a08
aab626 <hostile.command@gmail.com>
2025-05-01 13:31:03 -0400

    WIP user register form verificators
    Added missing city field in User model, changed email to be unique across users, changed some of the max lengths of other fields in the User model. In progress: data verifiers for the user input.

--- vendelia/marketplace/forms.py
+++ vendelia/marketplace/forms.py
@@ -10,12 +10,35 @@ class UserRegisterForm(UserCreationForm):
             'username',  'email',  'password1', 'password2', 
 
             # Provided by the custom User model
-            'phone_number', 'first_name', 'last_name'
+            'phone_number', 'first_name', 'last_name', 'city'
             ]
         
         # Custom field label overrides
         labels = {
+            'username': 'Usuario',
+            'email': 'Correo electrónico',
             'phone_number': 'Teléfono',
             'first_name': 'Nombre',
-            'last_name': 'Apellido'
+            'last_name': 'Apellido',
+            'city': 'Ciudad',
         }
+
+    # Username validator
+    # Length must be between 6 to 32 chars
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not (6 <= len(username) <= 32):
+            raise forms.ValidationError('El nombre de usuario debe tener entre 6 y 32 caracteres.')
+        
+        return username
+    
+    # Phone number validator
+    # Must be exactly 8 digits (only chilean cellphone numbers)
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if len(phone_number) != 8:
+            raise forms.ValidationError('El número debe consistir de 8 dígitos.')
+        elif not phone_number.isdigit():
+            raise forms.ValidationError('El número debe consistir de solo dígitos.')
+        
+        return phone_number
--- vendelia/marketplace/models.py
+++ vendelia/marketplace/models.py
@@ -5,6 +5,13 @@ from django.contrib.auth.models import AbstractUser
 # This model represents all the Users in Vendelia
 # It also has: username, password and email fields, given by its parent class.
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    # AbstractUser overrides:
+    #   username: max length
+    #   email: max length, must be unique
+    username = models.CharField(max_length=32, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    
+    phone_number = models.CharField(max_length=8)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    city = models.CharField(max_length=32)
