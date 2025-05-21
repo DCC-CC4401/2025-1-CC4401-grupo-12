30030d653bab22b7223805b34fe863bd71ade091
Monkchido <monkchi2@gmail.com>
2025-05-20 22:39:12 -0400

    Added a function for product details

--- vendelia/marketplace/views.py
+++ vendelia/marketplace/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from forms import ProductForm
-
+from models import Product
 # Create your views here.
 
 def register_product(request):
@@ -13,3 +13,7 @@ def register_product(request):
         form = ProductForm()
     return render(request, 'register_product.html', {'form': form})
 
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
