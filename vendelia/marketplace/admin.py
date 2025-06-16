f1be5c31686d2657728be50bb493b0c520735654
Jackeline Melady Peña <jackelinemeladypena07@gmail.com>
2025-06-15 22:36:20 -0500

    feat: modelo Compra registrado en admin.py

--- vendelia/marketplace/admin.py
+++ vendelia/marketplace/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Categoria, Product
+from .models import User, Categoria, Product, Compra
 
 # Register marketplace app models to admin panel
 admin.site.register(User)
 admin.site.register(Categoria)
 admin.site.register(Product)
+admin.site.register(Compra)
