384226a942cea0b25a727d25a66cac6944386772
Natalia Valencia <natalia_valencia@Natalias-MacBook-Air.local>
2025-06-29 20:54:57 -0400

    feat: Agregar validaciones de registro de producto. No agrego para fotos

--- vendelia/marketplace/forms.py
+++ vendelia/marketplace/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from django.core.exceptions import ValidationError
 from .models import User, Product
+import re
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
@@ -75,16 +76,36 @@ class ProductRegisterForm(forms.ModelForm):
                   'photos': 'Fotos'
         }
     
-    # Price validator:
+    # Price validator
     # The price can't be negative
     def clean_price(self):
         price = self.cleaned_data.get('price')
-        print('clean price')
         if price<0 and price is not None:
-            print('El precio no puede ser negativo')
             raise forms.ValidationError('El precio no puede ser negativo')
         
         return price
+    
+    # Name validator
+    # The name can't contain only empty spaces or the characters #, $ y %
+    def clean_product_name(self):
+        product_name = self.cleaned_data.get('product_name')
+        if not product_name.strip():
+            raise forms.ValidationError("El nombre no puede contener solo espacios")
+        if re.search(r"[#$%]", product_name):
+            raise forms.ValidationError("El nombre contiene caracteres no válidos (#, $, %)")
+        return product_name
+    
+    # Description validator
+    # The description can´t be made of only empty spaces
+    # Also, the description can't have the folllowing characters: #, $ y %
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if not description.strip():
+            raise forms.ValidationError("La descripción no puede sólo contener espacios en blanco")
+        if re.search(r"[#$%]", description):
+            raise forms.ValidationError("")
+        return description
+
 
 # Form to authenticate the user with email and password
 class EmailAuthenticationForm(forms.Form):
